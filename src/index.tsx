6fe700bc995baf13aea973c80ef3170835bf6c5b
Austine Were <austinewuncler@gmail.com>
2022-05-14 02:35:05 +0300

    build: configure build process

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import './index.scss';
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+createRoot(document.getElementById('app') as HTMLElement).render(<App />);
