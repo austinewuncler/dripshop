eb93c59f2d2c5b9741c9a76be2f90a36858aa99c
Austine Were <austinewuncler@gmail.com>
2022-05-14 05:36:26 +0300

    feat: apply background transition to directory items

--- src/components/DirectoryItem.tsx
+++ src/components/DirectoryItem.tsx
@@ -4,12 +4,15 @@ type DirectoryItemProps = { imageUrl: string; size?: string; title: string };
 
 const DirectoryItem = ({ imageUrl, size, title }: DirectoryItemProps) => (
   <article
-    className={`flex items-center justify-center flex-auto overflow-hidden bg-center bg-cover directory-item h-60 ${size}`}
-    style={{
-      backgroundImage: `url(${imageUrl})`,
-    }}
+    className={`flex items-center justify-center flex-auto overflow-hidden directory-item h-60 hover:cursor-pointer ${size}`}
   >
-    <div className="flex flex-col items-center justify-center content">
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
+    <div className="absolute flex flex-col items-center justify-center bg-white content opacity-70">
       <h1 className="font-bold title mb-1.5 uppercase">{title}</h1>
       <h2 className="font-thin uppercase subtitle">Shop Now</h2>
     </div>
